449d95c0e49341bc8002ffc1c98a7a11a8b633db
Sammy Lisa <lisa.5@osu.edu>
2020-05-11 16:52:46 -0400

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Samuel Lisa on 5/11/20.
+//  Copyright © 2020 Samuel Lisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
