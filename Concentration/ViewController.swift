4b8c876220940197a16d30a35adb2e9e9c96a074
Sammy Lisa <lisa.5@osu.edu>
2020-05-11 19:12:01 -0400

    lecture 1 - creating cards, card flips

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var emojiChoices = ["⛄️", "🥞", "🌶"]
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardIndex = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardIndex], on: sender)
+        } else {
+            print("card not found")
+        }
     }
-
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 }
 
